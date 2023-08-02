a807afbbd93b84a2af720b24d45c32cb5eeb3d26
lenne sebastien <sebastienlenne@gmail.com>
2023-08-02 09:48:49 +0200

    add file_storage for task 5 of project 2 airbnb clone V2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,18 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    new_dict[key] = value
+            return new_dict
+        else:
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +57,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete (self, obj=None):
+        """Delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__,__name__ + '.' + obj.id
+            if key in self.all():
+                del self.all()[key]
+                self.save()
