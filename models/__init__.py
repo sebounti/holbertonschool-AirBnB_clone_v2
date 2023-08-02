8a7823ab33276d6a6d8e169583f6839a60ad9d58
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-02 09:38:39 +0000

    task 6 useless classes import

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.user import User
-from models.review import Review
-from models.city import City
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.base_model import BaseModel
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -15,9 +8,5 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
 else:
     from models.engine import file_storage
     storage = file_storage.FileStorage()
-
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review,
-           "State": State, "User": User}
 
 storage.reload()
