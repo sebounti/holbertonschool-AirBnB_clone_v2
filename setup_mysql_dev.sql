cc0b8f203782f28040439e662f2df4a382e75af1
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-02 16:33:28 +0000

    task syntax fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- MySQL setup server
-CREATE DATABASE hbnb_dev_db
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO hbnb_dev@localhost;
-GRANT SELECT ON `performance_schema`.* TO hbnb_dev@localhost;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
