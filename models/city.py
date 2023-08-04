8013dda7eedf228576dfbc81ee2e8551d734f574
lenne sebastien <sebastienlenne@gmail.com>
2023-08-04 14:00:03 +0200

    correction file city.py review.py error fix

--- models/city.py
+++ models/city.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+from base_model import BaseModel, Base
 from os import getenv
-import models
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -12,6 +11,6 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('state_id'), nullable=False)
+        state_id = Column(String(60), ForeignKey('states_id'), nullable=False)
         places = relationship('Place', backref='cities',
                               cascade='all, delete, delete-orphan')
--- models/review.py
+++ models/review.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from base_model import BaseModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "Review"
+    __tablename__ = "Reviews"
     place_id = Column(String(60), nullable=False)
     user_id = Column(String(60), nullable=False)
     text = Column(String(1024), nullable=False)
