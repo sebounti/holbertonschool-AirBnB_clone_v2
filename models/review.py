b59dff51e2c7e12c73a048fe04b7605c114499af
lenne sebastien <sebastienlenne@gmail.com>
2023-08-04 14:02:22 +0200

    correction file  review.py error fix

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "Reviews"
+    __tablename__ = "Review"
     place_id = Column(String(60), nullable=False)
     user_id = Column(String(60), nullable=False)
     text = Column(String(1024), nullable=False)
