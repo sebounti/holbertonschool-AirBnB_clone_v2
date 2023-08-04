f0ef13243e36a556dd5da85f2fccca2afaae78d2
lenne sebastien <sebastienlenne@gmail.com>
2023-08-04 11:21:12 +0200

    update file review.py

--- models/review.py
+++ models/review.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-import models
 from models.base_model import BaseModel
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
@@ -8,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 class Review(BaseModel):
     """ Review classto store review information """
-    __name__ = "Review"
+    __tablename__ = "Review"
     place_id = Column(String(60), nullable=False)
     user_id = Column(String(60), nullable=False)
     text = Column(String(1024), nullable=False)
