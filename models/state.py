d41f81bf53b68958559405f55065d31adfe031ec
lenne sebastien <sebastienlenne@gmail.com>
2023-08-04 14:35:26 +0200

    correction review.py state.py  error eritage fix

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 import models
 from sqlalchemy import Column, String
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
