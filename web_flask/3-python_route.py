47a7de0d574aa7bc66294e43b0cd2147f1ec26ef
lenne sebastien <sebastienlenne@gmail.com>
2023-08-14 22:00:41 +0200

    add file task 3 for project web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"Script"
+'''script that starts a Flask web application'''
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_print(text):
+    result = text.replace('_', ' ')
+    return 'C {}'.format(result)
 
 
+@app.route("/python/<text>", strict_slashes=False)
+def python_print(text):
+    result = text.replace('_', ' ')
+    return 'Python {}'.format(result)
 
 
 if __name__ == '__main__':
