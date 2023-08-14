eca87f533934284e30b4f6eb7752e13fdcc62f3d
lenne sebastien <sebastienlenne@gmail.com>
2023-08-14 15:13:54 +0200

    add file 1- hbnb_route.py for project web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"Script"
+'''script that starts a Flask web application'''
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
