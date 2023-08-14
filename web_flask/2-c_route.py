99747e7734612f17d31dfbefb480ace84e78b47f
lenne sebastien <sebastienlenne@gmail.com>
2023-08-14 15:19:00 +0200

    add file 2-c-route.py  for project web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,13 +5,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
