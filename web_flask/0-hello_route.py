5ed4b1f8d1e349285d6096b66c1cbea152102244
lenne sebastien <sebastienlenne@gmail.com>
2023-08-16 15:56:39 +0200

    update file task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    return "Bonjour HBNB !"
+def Hello_HBNB():
+    return "Hello HBNB !"
 
 
 if __name__ == '__main__':
